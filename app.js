fd6ef950db2fa117bf3a9cfd5f69b284a6e5d55b
Evelynpa <evelyn.parra.rojas@gmail.com>
2017-12-27 20:43:13 -0300

    se realiza mejoras a codigo

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ String.fromCharCode(72) // 'H' **** ingresa el acsii y devuelve la letra*
 		var respuesta = prompt("Indique el número de la función a realizar: 1)Cifrar - 2)Descifrar");
 		if(respuesta != ""){
 			if(respuesta == "1") {
-				cifrado();
+				cipher();
 			} else if (respuesta == "2") {
-				decifrado();
+				decipher();
 			} else {
 				alert("Ingrese una opción válida");
 			}
@@ -20,172 +20,85 @@ String.fromCharCode(72) // 'H' **** ingresa el acsii y devuelve la letra*
 }
 
 	
-function cifrado(){
-	var textOriginal = prompt('Ingrese una frase a cifrar');
-function cipher(word){
 	
+function cipher(){
+	var textOriginal = prompt('Ingrese una frase a cifrar');
 var letter = '';
 //var espacio = / /;
-if(!isNaN(word)|| word.length < 0){//primero verifica si se ingresa numero o si la variable esta vacia
+if(!isNaN(textOriginal)){//primero verifica si se ingresa numero o si la variable esta vacia
 	alert('Debe ingresar letras!');
 }
 
-	else if(word === word.toUpperCase()){//determina si la cadena esta en mayuscula
+	else if(textOriginal === textOriginal.toUpperCase()){//determina si la cadena esta en mayuscula
 
-		for(var i = 0; i< word.length;i++){//itera letra por letra la frase
-			/*if(espacio.test(word.charAt(i))){//determina si una experesion regular existe en la iteracion
-				letter += word.charAt(i);//si existe que la mantenga
+		for(var i = 0; i< textOriginal.length;i++){//itera letra por letra la frase
+			/*if(espacio.test(textOriginal.charAt(i))){//determina si una experesion regular existe en la iteracion
+				letter += textOriginal.charAt(i);//si existe que la mantenga
 			}*/
-			var numeroAcsii = word.charCodeAt([i]);//muestra a cada letra su valor en ascii
+			var numeroAcsii = textOriginal.charCodeAt([i]);//muestra a cada letra su valor en ascii
 			var result = (numeroAcsii - 65 + 33)%26 + 65;//ejecuta la formula del codigo cesar
 			var textoCifrado  = String.fromCharCode(result);//el valor ascii lo cambia a letra
 
 			letter += textoCifrado;//cada letra la va concatenando a la nueva variable
 			
 		}
+		document.write('Su palabra cifrada es: <strong>'+ letter +'</strong>');//muestra la frase cifrada
 	}else{
-		for(var i = 0; i< word.length;i++){//itera letra por letra la frase
-			/*if(espacio.test(word.charAt(i))){//determina si una experesion regular existe en la iteracion
-				letter += word.charAt(i);//si existe que la mantenga
+		for(var i = 0; i< textOriginal.length;i++){//itera letra por letra la frase
+			/*if(espacio.test(textOriginal.charAt(i))){//determina si una experesion regular existe en la iteracion
+				letter += textOriginal.charAt(i);//si existe que la mantenga
 			}*/
-			var numeroAcsii = word.charCodeAt([i]);//muestra a cada letra su valor en ascii
+			var numeroAcsii = textOriginal.charCodeAt([i]);//muestra a cada letra su valor en ascii
 			var result = (numeroAcsii - 97 + 33)%26 + 97;//ejecuta la formula del codigo cesar
 			var textoCifrado  = String.fromCharCode(result);//el valor ascii lo cambia a letra
 
 			letter += textoCifrado.toLowerCase();//cada letra la va concatenando a la nueva variable
 		}
-
+		document.write('Su palabra cifrada es: <strong>'+ letter +'</strong>');//muestra la frase cifrada
 	}
 
-document.write('Su palabra cifrada es: <strong>'+ letter +'</strong>');//muestra la frase cifrada
+
+//llamo a la funcion con la variable que se creo en el prompt
+
 }
 
-cipher(textOriginal);//llamo a la funcion con la variable que se creo en el prompt
-}
-function decifrado(){
+
+	
+function decipher(){
 	var textCifrado = prompt('Ingrese una frase a descifrar');
-function decipher(word){
 var letter = '';
 //var espacio = / /;
 
-if(!isNaN(word)|| word.length < 0){//primero verifica si se ingresa numero o si la variable esta vacia
+if(!isNaN(textCifrado)|| textCifrado.length < 0){//primero verifica si se ingresa numero o si la variable esta vacia
 	alert('Debe ingresar letras!');
 }
-	else if(word === word.toUpperCase()){//determina si la cadena esta en mayuscula
+	else if(textCifrado === textCifrado.toUpperCase()){//determina si la cadena esta en mayuscula
 
-		for(var i = 0; i< word.length;i++){//itera letra por letra la frase
-			/*if(espacio.test(word.charAt(i))){//determina si una experesion regular existe en la iteracion
-				letter += word.charAt(i);//si existe que la mantenga
+		for(var i = 0; i< textCifrado.length;i++){//itera letra por letra la frase
+			/*if(espacio.test(textCifrado.charAt(i))){//determina si una experesion regular existe en la iteracion
+				letter += textCifrado.charAt(i);//si existe que la mantenga
 			}*/
-			var numeroAcsii = word.charCodeAt([i]);//muestra a cada letra su valor en ascii
+			var numeroAcsii = textCifrado.charCodeAt([i]);//muestra a cada letra su valor en ascii
 			var result = (numeroAcsii + 65 - 33)%26 + 65;//ejecuta la formula del codigo cesar
 			var textoCifrado  = String.fromCharCode(result);//el valor ascii lo cambia a letra
 
 			letter += textoCifrado;//cada letra la va concatenando a la nueva variable
 		}
+		document.write('Su palabra descifrada es: <strong>'+ letter +'</strong>');//muestra la frase cifrada
 	}else{
-		for(var i = 0; i< word.length;i++){//itera letra por letra la frase
-			/*if(espacio.test(word.charAt(i))){//determina si una experesion regular existe en la iteracion
-				letter += word.charAt(i);//si existe que la mantenga
+		for(var i = 0; i< textCifrado.length;i++){//itera letra por letra la frase
+			/*if(espacio.test(textCifrado.charAt(i))){//determina si una experesion regular existe en la iteracion
+				letter += textCifrado.charAt(i);//si existe que la mantenga
 			}*/
-			var numeroAcsii = word.charCodeAt([i]);//muestra a cada letra su valor en ascii
+			var numeroAcsii = textCifrado.charCodeAt([i]);//muestra a cada letra su valor en ascii
 			var result = (numeroAcsii + 85 - 33)%26 + 97;//ejecuta la formula del codigo cesar
 			var textoCifrado  = String.fromCharCode(result);//el valor ascii lo cambia a letra
 
 			letter += textoCifrado.toLowerCase();//cada letra la va concatenando a la nueva variable
 		}
+		document.write('Su palabra descifrada es: <strong>'+ letter +'</strong>');//muestra la frase cifrada
 }
 
-document.write('Su palabra descifrada es: <strong>'+ letter +'</strong>');//muestra la frase cifrada
-}
-decipher(textCifrado);
+
 }
 principal();
-/*********************FUNCION CIFRADO****************************************/
-/*
-var textOriginal = prompt('Ingrese una frase a cifrar');
-
-function cipher(word){
-var letter = '';
-if(!isNaN(word)|| word.length < 0){//primero verifica si se ingresa numero o si la variable esta vacia
-	alert('Debe ingresar letras!');
-}
-	else if(word === word.toUpperCase()){//determina si la cadena esta en mayuscula
-
-		for(var i = 0; i< word.length;i++){//itera letra por letra la frase
-			var numeroAcsii = word.charCodeAt([i]);//muestra a cada letra su valor en ascii
-			var result = (numeroAcsii - 65 + 33)%26 + 65;//ejecuta la formula del codigo cesar
-			var textoCifrado  = String.fromCharCode(result);//el valor ascii lo cambia a letra
-
-			letter += textoCifrado;//cada letra la va concatenando a la nueva variable
-		}
-	}else{
-		for(var i = 0; i< word.length;i++){//itera letra por letra la frase
-			var numeroAcsii = word.charCodeAt([i]);//muestra a cada letra su valor en ascii
-			var result = (numeroAcsii - 97 + 33)%26 + 97;//ejecuta la formula del codigo cesar
-			var textoCifrado  = String.fromCharCode(result);//el valor ascii lo cambia a letra
-
-			letter += textoCifrado.toLowerCase();//cada letra la va concatenando a la nueva variable
-		}
-}
-
-console.log(letter);//muestra la frase cifrada
-}
-
-cipher(textOriginal);//llamo a la funcion con la variable que se creo en el prompt
-*/
-
-
-/*********************FUNCION DECIFRADO****************************************/
-/*
-var textCifrado = prompt('Ingrese una frase a descifrar');
-
-function decipher(word){
-	var letter = '';
-
-
-for(var i = 0; i< word.length;i++){
-	var numeroAcsii = word.charCodeAt([i]);
-	var result = (numeroAcsii + 65 - 33)%26 + 65;
-	var textoCifrado  = String.fromCharCode(result);
-
-	letter += textoCifrado;
-}
-
-console.log(letter);
-}
-
-decipher(textCifrado);
-*/
-/*
-
-function decipher(word){
-	var textCifrado = prompt('Ingrese una frase a descifrar');
-
-var letter = '';
-if(!isNaN(word)|| word.length < 0){//primero verifica si se ingresa numero o si la variable esta vacia
-	alert('Debe ingresar letras!');
-}
-	else if(word === word.toUpperCase()){//determina si la cadena esta en mayuscula
-
-		for(var i = 0; i< word.length;i++){//itera letra por letra la frase
-			var numeroAcsii = word.charCodeAt([i]);//muestra a cada letra su valor en ascii
-			var result = (numeroAcsii + 65 - 33)%26 + 65;//ejecuta la formula del codigo cesar
-			var textoCifrado  = String.fromCharCode(result);//el valor ascii lo cambia a letra
-
-			letter += textoCifrado;//cada letra la va concatenando a la nueva variable
-		}
-	}else{
-		for(var i = 0; i< word.length;i++){//itera letra por letra la frase
-			var numeroAcsii = word.charCodeAt([i]);//muestra a cada letra su valor en ascii
-			var result = (numeroAcsii + 85 - 33)%26 + 97;//ejecuta la formula del codigo cesar
-			var textoCifrado  = String.fromCharCode(result);//el valor ascii lo cambia a letra
-
-			letter += textoCifrado.toLowerCase();//cada letra la va concatenando a la nueva variable
-		}
-}
-
-console.log(letter);//muestra la frase cifrada
-}
-decipher(textCifrado);
-*/
